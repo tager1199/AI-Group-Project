dcd263946845aa6dc17aee11f2e0fc2dd8022980
tager1199 <32684792+tager1199@users.noreply.github.com>
2019-03-29 06:31:43 +0000

    game.cs finished
    game.cs completed, AI updated and extra files removed

---
+++ Battleships AI/Battleships/battleships/AiGuess.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+
+namespace AIbattleships
+{
+    class AiGuess
+    {
+        public string[][] grid;
+
+        public AiGuess(){
+
+            string[] lines = File.ReadAllLines(@"C:\Users\Student\Documents\GitHub\AI-Group-Project\Battleships AI\Battleships AI\AIPoints.txt");
+            int n = 0;
+            foreach (string line in lines)
+            {
+                grid[n] = (line.Split(' '));
+                n += 1;
+            }
+            Console.WriteLine(grid);
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+
+}
+}
