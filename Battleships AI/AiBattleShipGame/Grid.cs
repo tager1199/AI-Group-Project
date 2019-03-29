840e021d05d8ddc2076916a83436f58f2f8fb379
tager1199 <176433144@students.lincoln.ac.uk>
2019-03-29 11:45:53 +0000

    Working Files

---
+++ Battleships AI/AiBattleShipGame/Grid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiBattleShipGame
+{
+    class Grid
+    {
+        public char[,] values;
+        public int sizeX = 10;
+        public int sizeY = 10;
+
+        public Grid()
+        {
+            values = new char[sizeX, sizeY];
+            for (int i = 0; i < values.GetLength(0); i++)
+                for (int j = 0; j < values.GetLength(1); j++)
+                    values[i, j] = ' ';
+        }
+    }
+}
